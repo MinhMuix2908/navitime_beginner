5a1427c7200af5f658f12117dd370082a2937322
minh.daoquang <minh.daoquang@vti.com.vn>
2024-07-22 17:27:16 +0700

    Add first commit

--- routes/carController.js
+++ routes/carController.js
@@ -2,20 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { findSpotAndAddress, detailCarMap, drawCarMap } = require('../handlers/carHandler')
 
-router.get('/draw_car_map', async (req, res) => {
-  try {
-    var { start, goal, transits } = req.query;
-    var startList = start ? start.split(',') : []
-    var endList = goal ? goal.split(',') : []
-    var transitsList = transits ? transits.split(',') : [];
-
-    var shapeCar = await drawCarMap(start, goal, transitsList);
-    res.render('drawCarMap', { shapeCar: shapeCar, startCoordinates: startList, goalCoordinates: endList, coordinatesList: transitsList });
-  } catch (error) {
-    console.error('Error in /draw_car_map route:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
 
 router.get('/draw_car_map_by_navitime', async (req, res) => {
   try {
--- routes/rootController.js
+++ routes/rootController.js
@@ -7,7 +7,6 @@ router.get('/', async (req, res) => {
       {name: '[HTML]map: default', url: '/map/default', description: 'Get the default map of Japan'},
       {name: '[HTML]map: point', url: '/map/point?lat=35.632548&lon=139.881327', description: 'Display a specific point when coordinates are provided'},
       {name: '[API]car: find_spot', url: '/car/find_spot?word=tok', description: 'Search for some points on the map by providing the name of the point'},
-      {name: '[HTML]car: draw_car_map', url: '/car/draw_car_map?start=35.632548,139.881327&goal=35.673581,139.760239&transits=35.679769,139.807961,35.666007,139.717035', description: 'Draw a car route from point A to B, with intermediate stops at C and D. Provide the coordinates of the points'},
       {name: '[HTML]car: draw_car_map_by_navi_time', url: '/car/draw_car_map_by_navitime?start=35.632548,139.881327&goal=35.673581,139.760239&transits=35.679769,139.807961,35.666007,139.717035', description: 'Draw a car route from point A to B, with intermediate stops at C and D. Provide the coordinates of the points'},
       {name: '[API]car: detail_car_map', url: '/car/detail_car_map?start=35.632548,139.881327&goal=35.673581,139.760239&transits=35.679769,139.807961,35.666007,139.717035', description: 'Data for a car route from point A to B, with intermediate stops at C and D. Provide the coordinates of the points'},
       {name: '[API]bus: find_station', url: '/bus/find_station?word=tok', description: 'Search for some bus stops. Provide the name of the stop'},
